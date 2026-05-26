929f0712eac95d054b2c498df16a0b0c78fbaf82
Dev Raval <devraval777dr@gmail.com>
2026-05-26 18:49:37 +0530

    Just start to build backend of user authentication

--- frontend/frontend/src/PhoneLogin.jsx
+++ frontend/frontend/src/PhoneLogin.jsx
@@ -1,7 +1,14 @@
 import "./PhoneLogin.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 export default function PhoneLogin() {
+  const [number,setNumber] = useState("");
+  const onSumbit = async()=>{
+    const response = await fetch()
+  }
   return (
     <div className="container">
       <div className="card">
@@ -10,16 +17,33 @@ export default function PhoneLogin() {
           Enter your E-mail to use the application.
         </p>
 
-        <label>Email</label>
+        
+        
 
-        <div className="call-container">
+        <div className="call-container" >
+          <label htmlFor="email">Email</label>
 
           <input
             type="text"
             className="right-small-card"
             placeholder="Enter your Email"
+            name="email"
+            onClick={(e)=>setNumber(e.target.value)}
           />
+          <br />
+          <br />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" className="right-small-card" placeholder="Enter your password" />
         </div>
+        
+        <div>
+          <FontAwesomeIcon
+            icon={faEye}
+            style={{ color: "black" }}
+            />
+        </div>
+
+
 
         <button className="btn">Next</button>
         <h6>New user <Link to="/signup" id="link">Create Account here</Link></h6>
